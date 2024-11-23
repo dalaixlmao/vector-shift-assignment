27b2c51cdee7b3ec3378dbc88fc92e3188263ec0
Anubhav <anubhav_a@me.iitr.ac.in>
2024-11-23 15:15:31 +0530

    Improved styling

--- frontend/src/components/node-wrapper.jsx
+++ frontend/src/components/node-wrapper.jsx
@@ -46,12 +46,12 @@ export default function NodeWrapper({
   };
   return (
     <div
-      className={`text-white bg-violet-900 relative flex flex-col gap-2 pt-2 rounded-xl h-full`}
+      className={`text-white border-x-[1.5px] border-violet-500 border-b border-b-white/10 bg-violet-900 relative flex flex-col gap-2 pt-2 rounded-xl h-full`}
     >
       <div className="px-2 font-medium">
         {title || type}
       </div>
-      <div className="text-sm py-2 font-light h-full rounded-lg px-4 pb-4 bg-violet-950">
+      <div className="text-sm py-2 font-light h-full rounded-xl px-4 pb-4 bg-violet-950">
         {renderHandles()}
         {children}
       </div>
